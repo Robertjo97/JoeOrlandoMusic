5346e27e287398c3f353b0245cdfc7644bd7a311
Robertjo97 <robertjo97@gmail.com>
2024-07-24 11:33:27 -0700

    Add icons

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,14 +2,16 @@ import Header from "../components/Header";
 import NavBar from "../components/Navbar";
 import Hero from "./Hero";
 import Image from "./Image";
+import Socials from "./Socials";
 
-let Home = () => {
+const Home = () => {
   return (
     <div className="container">
       <Header />
       <NavBar />
       <Image />
       <Hero />
+      <Socials/>
     </div>
   );
 };
