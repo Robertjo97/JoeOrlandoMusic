a5ed64276939f47864841eb57e4c445feba334df
Robertjo97 <robertjo97@gmail.com>
2024-07-31 11:25:43 -0700

    style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 let Header = () => {
-  return <div className="header">Joe Orlando Music</div>;
+  return <div className="header"><a href="#">Joe Orlando Music</a></div>;
 };
 
 export default Header;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ const NavBar = () => {
   return (
     <nav>
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Shows</li>
-        <li>Music</li>
-        <li>Contact</li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Shows</a></li>
+        <li><a href="#">Music</a></li>
+        <li><a href="#">Contact</a></li>
       </ul>
     </nav>
   );
