a2a43817a7c4a47555b8523d6f0b0069f6ce11ef
Robertjo97 <robertjo97@gmail.com>
2024-08-02 17:54:29 -0700

    setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import NavBar from "./components/Navbar.jsx";
 import Home from "./Home/Home.jsx";
 
 function App() {
   return (
-    <Home/>
+    <div>
+      <NavBar/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/shows" component={Shows}/>
+        <Route path="/music" component={Music}/>
+        <Route path="/contact" component={Contact}/>
+      </Switch>
+    </div>
   );
 }
 
