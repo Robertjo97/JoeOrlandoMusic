ca20dd2717d686f38153b9f24e173a96ab5849f2
Robertjo97 <robertjo97@gmail.com>
2024-08-03 15:50:58 -0700

    modify about

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,12 +1,13 @@
 import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
-
+import Paragraph from "./Paragraph";
 import "./about.css";
 
 const About = () => {
   return (
     <div className="about-container">
       <NavBar />
+      <Paragraph />
       <Footer />
     </div>
   );
