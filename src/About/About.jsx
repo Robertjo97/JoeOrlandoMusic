a610cfaa9e24c676a26a4f570ba6c2f5abe28588
Robertjo97 <robertjo97@gmail.com>
2024-08-02 18:58:00 -0700

    refactor css

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,9 +1,10 @@
 import Header from "../components/Header";
 import NavBar from "../components/Navbar";
+import "./about.css";
 
 const About = () => {
   return (
-    <div className="container">
+    <div className="about-container">
       <div className="top">
         <Header />
         <NavBar />
--- src/Home/Hero.jsx
+++ src/Home/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <div className="hero-container">
-      <p>Next Show:</p>
+      <p>Upcoming Show:</p>
       <p>October 19th |  2:00pm - 10:30pm |  Target Clovis, CA</p>
     </div>
   );
