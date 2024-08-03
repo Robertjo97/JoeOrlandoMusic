c343e4b3895185494c6a9fe23bfc665e6aeca31a
Robertjo97 <robertjo97@gmail.com>
2024-08-03 15:11:48 -0700

    remove deprecated header

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,15 +1,13 @@
-import Header from "../components/Header";
 import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
+
 import "./about.css";
 
 const About = () => {
   return (
     <div className="about-container">
-      <div className="top">
-        <Header />
-        <NavBar />
-      </div>
-      <p>About page</p>
+      <NavBar />
+      <Footer />
     </div>
   );
 };
--- src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-import {Link} from 'react-router-dom';
-import "./Header.css";
-
-let Header = () => {
-  return <div className="header"><Link to="/">Joe Orlando Music</Link></div>;
-};
-
-export default Header;
