42f60684877b8179239fbfb34b41510423f31b30
Robertjo97 <robertjo97@gmail.com>
2024-08-02 19:17:38 -0700

    modify hero

--- src/Home/Hero.jsx
+++ src/Home/Hero.jsx
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <div className="hero-container">
       <p>Upcoming Show:</p>
-      <p>October 19th |  2:00pm - 10:30pm |  Target Clovis, CA</p>
+      <p>October 19th | 12:00am - 12:00pm | Fresno, CA</p>
     </div>
   );
 };
