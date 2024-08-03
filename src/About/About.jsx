2b1bb57619cc695bcf680d519a2f5d146d92b36f
Robertjo97 <robertjo97@gmail.com>
2024-08-02 18:44:13 -0700

    mobile styling

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,5 +1,16 @@
+import Header from "../components/Header";
+import NavBar from "../components/Navbar";
+
 const About = () => {
-  return <h1>About Page</h1>;
+  return (
+    <div className="container">
+      <div className="top">
+        <Header />
+        <NavBar />
+      </div>
+      <p>About page</p>
+    </div>
+  );
 };
 
 export default About;
