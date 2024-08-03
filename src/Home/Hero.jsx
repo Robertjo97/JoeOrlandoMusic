4e45c7c6f2395e671c465fadc5445522bbf7a564
Robertjo97 <robertjo97@gmail.com>
2024-08-02 17:17:29 -0700

    date

--- src/Home/Hero.jsx
+++ src/Home/Hero.jsx
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <div className="hero-container">
       <p>Next Show:</p>
-      <p>Date Time Location</p>
+      <p>October 19th |  2:00pm - 10:30pm |  Target Clovis, CA</p>
     </div>
   );
 };
