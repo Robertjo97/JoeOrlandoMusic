25b538717b6bf55d85d5f2ecac8c381097de7d83
Robertjo97 <robertjo97@gmail.com>
2024-08-06 11:47:00 -0700

    style aboutus

--- src/About/AboutUs.jsx
+++ src/About/AboutUs.jsx
@@ -1,17 +1,22 @@
+import collective from "../assets/collective.png";
+
 const AboutUs = () => {
   return (
     <div className="aboutus-container">
-      <h3>About Us</h3>
-      <p>
-        The Collective was born out of a shared love for classic rock and a
-        desire to bring that timeless sound to new audiences. Formed in the
-        Sacramento Area, 2019, our band has grown from playing small local
-        venues to headlining festivals.
-      </p>
-      <p>
-        "we dont make it sound like the record. We come to play the music and
-        make it come alive"
-      </p>
+      <img src={collective}></img>
+      <div className="paragraph">
+        <h3>About Us</h3>
+        <p className="slogan">
+          "We dont make it sound like the record. We come to play the music and
+          make it come alive."
+        </p>
+        <p className="description">
+          The Collective was born out of a shared love for classic rock and a
+          desire to bring that timeless sound to new audiences. Formed in the
+          Sacramento Area, 2019, our band has grown from playing small local
+          venues to headlining festivals.
+        </p>
+      </div>
     </div>
   );
 };
