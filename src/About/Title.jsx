bdf9989b81bfdf7be5013f0f4e051e7593964c8f
Robertjo97 <robertjo97@gmail.com>
2024-08-05 17:07:15 -0700

    style title

--- src/About/Title.jsx
+++ src/About/Title.jsx
@@ -2,7 +2,7 @@ const Title = () => {
   return (
     <div className="title-container">
       <h1>The Collective</h1>
-      <h3>Rocking the Classics Since 2019</h3>
+      <h3>-Rocking the Classics Since 2019</h3>
     </div>
   );
 };
