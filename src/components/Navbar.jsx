1841176ee87b5c8572361fbd716cdba23413e9e5
Robertjo97 <robertjo97@gmail.com>
2024-08-16 15:10:58 -0700

    modify navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,30 +3,30 @@ import "./Navbar.css";
 
 const NavBar = () => {
   return (
-    <div className="nav-container">
-      <nav>
-        <div className="header">
-          <Link to="/">Joe Orlando Music</Link>
-        </div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+      <nav className="container-fluid navbar navbar-expand-sm d-flex justify-content-around align-items-center mb-5 border-bottom" data-bs-theme="dark">
+        <Link to="/" className="navbar-brand m-0">
+          <h1 className="display-5">Joe Orlando Music</h1>
+        </Link>
+        <ul className="navbar-nav d-flex flex-row">
+          <li className="nav-item">
+            <Link to="/" className="nav-link fs-5 text-white">
+              Home
+            </Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>
+          <li className="nav-item">
+            <Link to="/about" className="nav-link fs-5 text-white">About</Link>
           </li>
-          <li>
-            <Link to="/shows">Shows</Link>
+          <li className="nav-item">
+            <Link to="/shows" className="nav-link fs-5 text-white">Shows</Link>
           </li>
-          <li>
-            <Link to="/music">Music</Link>
+          <li className="nav-item">
+            <Link to="/music" className="nav-link fs-5 text-white">Music</Link>
           </li>
-          <li>
-            <Link to="/contact">Contact</Link>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-link fs-5 text-white">Contact</Link>
           </li>
         </ul>
       </nav>
-    </div>
   );
 };
 
