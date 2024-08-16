072c37572d04694da978aecf09f7ab3fd08a11c0
Robertjo97 <robertjo97@gmail.com>
2024-08-16 15:28:13 -0700

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,27 +3,27 @@ import "./Navbar.css";
 
 const NavBar = () => {
   return (
-      <nav className="container-fluid navbar navbar-expand-sm d-flex justify-content-around align-items-center mb-5 border-bottom" data-bs-theme="dark">
+      <nav className="container-fluid navbar navbar-expand-sm d-flex justify-content-around align-items-center mb-5 border-bottom bg-dark-subtle shadow-lg">
         <Link to="/" className="navbar-brand m-0">
           <h1 className="display-5">Joe Orlando Music</h1>
         </Link>
         <ul className="navbar-nav d-flex flex-row">
           <li className="nav-item">
-            <Link to="/" className="nav-link fs-5 text-white">
+            <Link to="/" className="nav-link fs-5">
               Home
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/about" className="nav-link fs-5 text-white">About</Link>
+            <Link to="/about" className="nav-link fs-5 ">About</Link>
           </li>
           <li className="nav-item">
-            <Link to="/shows" className="nav-link fs-5 text-white">Shows</Link>
+            <Link to="/shows" className="nav-link fs-5">Shows</Link>
           </li>
           <li className="nav-item">
-            <Link to="/music" className="nav-link fs-5 text-white">Music</Link>
+            <Link to="/music" className="nav-link fs-5">Music</Link>
           </li>
           <li className="nav-item">
-            <Link to="/contact" className="nav-link fs-5 text-white">Contact</Link>
+            <Link to="/contact" className="nav-link fs-5">Contact</Link>
           </li>
         </ul>
       </nav>
