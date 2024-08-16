cb5e1ba447501139b8610620c1dd3592a25cb76c
Robertjo97 <robertjo97@gmail.com>
2024-08-16 16:11:29 -0700

    modify hero

--- src/Home/Hero.jsx
+++ src/Home/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="hero-container text-center">
+    <div className="hero-container text-center text-body-emphasis fs-3">
       <p>Next Show:</p>
       <p>October 19th | 12:00am - 12:00pm | Fresno, CA</p>
     </div>
