9c5548d11f1f36a29dc93d6621a90a9b236dcddc
Robertjo97 <robertjo97@gmail.com>
2024-08-16 15:37:42 -0700

    fix img and ul padding

--- src/Home/Image.jsx
+++ src/Home/Image.jsx
@@ -3,7 +3,7 @@ import joeImage from "../assets/BIO-4.png";
 const Image = () => {
   return (
     <div className="image">
-      <img src={joeImage} alt="Picture of Joe Orlando"></img>
+      <img src={joeImage} className="rounded-3 shadow-lg border" alt="Picture of Joe Orlando"></img>
     </div>
   );
 };
