471208e1dc957763d7d864b2ff415cf8692c51f6
Robertjo97 <robertjo97@gmail.com>
2024-08-17 15:09:34 -0700

    modify footer

--- src/Home/Image.jsx
+++ src/Home/Image.jsx
@@ -2,7 +2,7 @@ import joeImage from "../assets/BIO-4.png";
 
 const Image = () => {
   return (
-    <div className="image">
+    <div className="image mb-5">
       <img src={joeImage} className="rounded-3 shadow-lg border" alt="Picture of Joe Orlando"></img>
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className="footer-container">
+    <div className="footer-container border-top">
       <footer>
         <p>© 2024 Joe Orlando Music</p>
       </footer>
