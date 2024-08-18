eebd24f037edcd6af0ddef2793b4e10f55e9e04e
Robertjo97 <robertjo97@gmail.com>
2024-08-17 18:25:46 -0700

    upcoming

--- src/Home/Hero.jsx
+++ src/Home/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <div className="container hero text-center mb-5">
-      <p className="display-6 fs-3 mb-4"><strong><em>Next Show:</em></strong></p>
+      <p className="display-6 fs-3 mb-4"><strong><em>Upcoming Show:</em></strong></p>
       <ul className="list-group list-group-flush display-6 fs-4">
         <li className="list-group-item pb-3">August 24th - 6:00pm</li>
         <li className="list-group-item p-3">Moonraker Millhouse, Cameron Park, CA</li>
