397642330c7372d0187e8f623548408447943589
Robertjo97 <robertjo97@gmail.com>
2024-08-17 17:33:46 -0700

    modify hero

--- src/Home/Hero.jsx
+++ src/Home/Hero.jsx
@@ -1,11 +1,11 @@
 const Hero = () => {
   return (
     <div className="container text-center mb-5">
-      <p className="display-6 fs-2">Next Show:</p>
+      <p className="display-6 fs-3 mb-4"><strong><em>Next Show:</em></strong></p>
       <ul className="list-group list-group-flush display-6 fs-4">
-        <li className="list-group-item">August 24th |  6:00pm - 9:00pm</li>
-        <li className="list-group-item">Moonraker Millhouse, Cameron Park, CA</li>
-        <li className="list-group-item">The Collective</li>
+        <li className="list-group-item pb-3">August 24th - 6:00pm</li>
+        <li className="list-group-item p-3">Moonraker Millhouse, Cameron Park, CA</li>
+        <li className="list-group-item pt-3">The Collective</li>
       </ul>
     </div>
   );
