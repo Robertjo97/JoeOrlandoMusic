56dfffb904fca4f41b10dd9d1bae034d99c27064
Robertjo97 <robertjo97@gmail.com>
2024-08-17 18:29:50 -0700

    modify hero font size

--- src/Home/Hero.jsx
+++ src/Home/Hero.jsx
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <div className="container hero text-center mb-5">
       <p className="display-6 fs-3 mb-4"><strong><em>Upcoming Show:</em></strong></p>
-      <ul className="list-group list-group-flush display-6 fs-4">
+      <ul className="list-group list-group-flush display-6 fs-5">
         <li className="list-group-item pb-3">August 24th - 6:00pm</li>
         <li className="list-group-item p-3">Moonraker Millhouse, Cameron Park, CA</li>
         <li className="list-group-item pt-3">The Collective</li>
