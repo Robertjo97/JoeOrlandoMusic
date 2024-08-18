2f1d19efef8236297f68be7fc72226832bb97d0b
Robertjo97 <robertjo97@gmail.com>
2024-08-17 19:29:55 -0700

    modify image

--- src/Home/Image.jsx
+++ src/Home/Image.jsx
@@ -3,7 +3,7 @@ import joeImage from "../assets/BIO-4.png";
 const Image = () => {
   return (
     <div className="image mb-5">
-      <img src={joeImage} className="rounded-3 shadow-lg border" alt="Picture of Joe Orlando"></img>
+      <img src={joeImage} className="rounded-3 border" alt="Picture of Joe Orlando"></img>
     </div>
   );
 };
