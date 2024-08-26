7c3bedbcac4c36521db2b7fb5bec60513c9ec989
Robertjo97 <robertjo97@gmail.com>
2024-08-26 08:59:13 -0700

    update shows

--- src/Shows/Shows.jsx
+++ src/Shows/Shows.jsx
@@ -21,16 +21,16 @@ const Shows = () => {
   return (
     <div className="shows-container vh-100 d-flex flex-column justify-content-between">
       <NavBar />
-      <div className="shows-list">
+      <div className="shows-list container mb-5">
         {shows.length > 0 ? (
-          <ul>
+          <ul className="list-group">
             {shows.map((show, index) => (
-              <li key={index}>
+              <li className="list-group-item bg-black text-center" key={index}>
                 {show.date} - {show.time}<br></br>{show.location}<br></br>{show.band}
               </li>
             ))}
           </ul>
-        ) : (<p>No upcoming shows. Please check back soon!</p>)}
+        ) : (<p className="text-center">No upcoming shows at this time. Please check back soon!</p>)}
       </div>
       <Footer />
     </div>
