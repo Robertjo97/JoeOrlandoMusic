f2ab0b1115e196a70c4f41f3e4f78f9f56a857df
Robertjo97 <robertjo97@gmail.com>
2024-09-01 14:24:44 -0700

    update todo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const NavBar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
         <div
           className="collapse navbar-collapse justify-content-end"
