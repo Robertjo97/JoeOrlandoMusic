ceac23d8ffcf1967ba1b286b06c66e754410750c
Robert Orlando <robertjo97@gmail.com>
2024-09-04 12:06:13 -0700

    increase hero font size

--- src/Home/Hero.jsx
+++ src/Home/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
     <div className="container">
       <div className="hero d-flex justify-content-center border-top border-bottom p-5 align-items-center gap-5 mb-5">
         <h1><em>Upcoming Show:</em></h1>
-        <ul className="list-group list-group-flush display-6 fs-4">
+        <ul className="list-group list-group-flush display-6 fs-3">
           <li className="list-group-item bg-black text-white"><span className="text-danger">*</span> {nextShow.date} - {nextShow.time}</li>
           <li className="list-group-item bg-black text-white"><span className="text-danger">*</span> {nextShow.location}</li>
           <li className="list-group-item bg-black text-white"><span className="text-danger">*</span> {nextShow.band}</li>
