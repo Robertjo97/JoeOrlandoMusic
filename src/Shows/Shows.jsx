695d57250ddd063a167899114e40aba51407b12c
Robert Orlando <robertjo97@gmail.com>
2024-09-05 11:22:43 -0700

    style shows

--- src/Shows/Shows.jsx
+++ src/Shows/Shows.jsx
@@ -18,18 +18,20 @@ const Shows = () => {
   }, []);
 
   return (
-    <div className="shows-container vh-100 d-flex flex-column justify-content-between">
+    <div className="shows-container vh-100 d-flex flex-column justify-content-between text-center">
       <NavBar />
       <div className="title mx-auto my-5">
         <h2>Come see us live!</h2>
-        
+
       </div>
       <div className="shows-list container mb-5 d-flex justify-content-center align-items-center">
         {shows.length > 0 ? (
           <ul className="list-group list-group-flush display-6 fs-4 mb-5">
             {shows.map((show, index) => (
               <li className="list-group-item bg-black p-4" key={index}>
-                <span className="text-danger">* </span>{show.date} - {show.time}<br></br><span className="text-danger">* </span>{show.location}<br></br><span className="text-danger">* </span>{show.band}
+                <p><span className="text-danger">* </span>{show.date} - {show.time}<span className="text-danger"> *</span></p>
+                <p><span className="text-danger">* </span>{show.location}<span className="text-danger"> *</span></p>
+                <p><span className="text-danger">* </span>{show.band}<span className="text-danger"> *</span></p>
               </li>
             ))}
           </ul>
