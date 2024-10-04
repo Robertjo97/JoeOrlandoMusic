c70ad6295272ef9fc62870d28838e8ac54dbba32
Robertjo97 <robertjo97@gmail.com>
2024-10-04 12:20:28 -0700

    mobile bugfix

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import NavBar from "../components/Navbar";
 import Hero from "./Hero";
 import Image from "./Image";
@@ -6,13 +5,6 @@ import Footer from "../components/Footer";
 import "./Home.css";
 
 const Home = () => {
-  useEffect(() => {
-    // Force reflow on load to fix the layout issue
-    document.body.style.display = "none";
-    document.body.offsetHeight; // Trigger reflow
-    document.body.style.display = "";
-  }, []); // Empty dependency array ensures this runs once on component mount
-
   return (
     <div className="home-container vh-100 d-flex flex-column justify-content-between">
       <NavBar />
