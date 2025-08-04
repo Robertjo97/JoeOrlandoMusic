a63cd7df777c3f2ffa1a16f678731dab8dd2d174
Robert Orlando <robertjo97@gmail.com>
2025-08-03 22:27:41 -0700

    Add custom field to hero.

--- src/Home/Hero.jsx
+++ src/Home/Hero.jsx
@@ -18,9 +18,10 @@ const Hero = () => {
         {nextShow && nextShow.date ? (
           <ul className="list-group list-group-flush display-6 fs-3 text-center">
             <p className="display-6 fs-2 mb-4"><em><strong>Upcoming Show:</strong></em></p>
+            <li className="list-group-item bg-black text-white"><span className="text-danger">*</span> {nextShow.band}<span className="text-danger"> *</span></li>
             <li className="list-group-item bg-black text-white">{nextShow.day} | {nextShow.date} | {nextShow.time}</li>
             <li className="list-group-item bg-black text-white"><em>{nextShow.location}</em></li>
-            <li className="list-group-item bg-black text-white"><span className="text-danger">*</span> {nextShow.band}<span className="text-danger"> *</span></li>
+            <li className="list-group-item bg-black text-white">{nextShow.custom && <p><span className="text-warning">* </span>{nextShow.custom}<span className="text-warning"> *</span></p>}</li>
           </ul>
         ) : (<p className="text-center">No upcoming shows at this time. Please check back soon!</p>)}
       </div>
