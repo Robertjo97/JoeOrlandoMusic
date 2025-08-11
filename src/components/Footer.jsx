1ffb4bac9b67c6eb3a518336f77b8bd73c34c9e4
Robertjo97 <robertjo97@gmail.com>
2025-08-10 19:04:00 -0700

    Add custom field and LK button.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,6 +33,11 @@ const Footer = () => {
                 <img className="icons" src={bandcamp} alt="Bandcamp link"></img>
               </a>
             </li>
+            <li>
+              <a href="https://www.facebook.com/landokoop#">
+                <div className="LK-container">Lando Koop</div>
+              </a>
+            </li>
           </ul>
         </div>
         <p>© 2024 Joe Orlando Music</p>
